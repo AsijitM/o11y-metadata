37cdc5166c324e9818123ccdc1056d5a96f55853
Asijit Manna <asijit@browserstack.com>
2025-05-12 14:57:11 +0530

    feat: add random element selection for cart pane display in test

--- tests/bstack-sample-test.py
+++ tests/bstack-sample-test.py
@@ -1,3 +1,4 @@
+import random
 import pytest
 from selenium.webdriver.common.by import By
 
@@ -10,8 +11,15 @@ def test_example(selenium):
     # clicking the 'Add to cart' button
     selenium.find_element(By.XPATH, '//*[@id="1"]/div[4]').click()
 
+    a = random.randint(1, 1000)
+    print(f'Asijit -----> {a}')
+    if int(a) % 2 == 0:
+        element_name = "float-cart__conten"
+    else :
+        element_name = "float-cart__content"
+
     # waiting until the Cart pane has been displayed on the webpage
-    selenium.find_element(By.CLASS_NAME, 'float-cart__content')
+    selenium.find_element(By.CLASS_NAME, element_name)
 
     # locating product in cart and getting name of the product in cart
     productCartText = selenium.find_element(By.XPATH, '//*[@id="__next"]/div/div/div[2]/div[2]/div[2]/div/div[3]/p[1]').text
