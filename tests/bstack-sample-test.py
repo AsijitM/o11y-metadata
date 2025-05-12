fec3ad508dc4ec8d0e73df949c683ca987ef2d01
Asijit Manna <asijit@browserstack.com>
2025-05-12 13:44:46 +0530

    update tests

--- tests/bstack-sample-test.py
+++ tests/bstack-sample-test.py
@@ -1,5 +1,8 @@
 import pytest
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 def test_example(selenium):
     selenium.get('https://bstackdemo.com/')
@@ -18,3 +21,25 @@ def test_example(selenium):
 
     # checking whether product has been added to cart by comparing product name
     assert productCartText == productText
+
+def test_product_sorting(selenium):
+    selenium.get('https://bstackdemo.com/')
+
+    # Find and click on the sort dropdown
+    sort_dropdown = selenium.find_element(By.CSS_SELECTOR, '.sort select')
+    select = Select(sort_dropdown)
+
+    # Select "Lowest to Highest" option
+    select.select_by_visible_text('Lowest to Highest')
+
+    # Wait for the page to update after sorting
+    WebDriverWait(selenium, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, '.shelf-item'))
+    )
+
+    # Get the prices of the first two items to verify sorting
+    first_item_price = float(selenium.find_element(By.XPATH, '//*[@id="1"]/div[3]/span').text.replace('$', ''))
+    second_item_price = float(selenium.find_element(By.XPATH, '//*[@id="2"]/div[3]/span').text.replace('$', ''))
+
+    # Verify that the sorting works correctly (first item should be cheaper than or equal to second)
+    assert first_item_price <= second_item_price, "Products are not sorted correctly by price"
