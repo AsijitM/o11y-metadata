a4763e07a6a62124b116bd462245d17dab465390
Asijit Manna <asijit@browserstack.com>
2025-05-13 10:38:57 +0530

    fix: correct expected title in test_local function

--- bstack-local-test.py
+++ bstack-local-test.py
@@ -3,4 +3,7 @@ import pytest
 def test_local(selenium):
   selenium.get('http://bs-local.com:45454')
   local_page_title = selenium.title
-  assert local_page_title == 'BrowserStack Local'
+  assert local_page_title == 'BrowserStack Locals'
+
+def test_example():
+  assert 2 + 2 == 4
